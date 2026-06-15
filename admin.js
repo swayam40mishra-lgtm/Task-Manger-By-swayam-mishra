ead794d750c1a29c00899cb2e0baedcae9be073e
swayam40mishra-lgtm <swayam40mishra@gmail.com>
2026-06-16 02:24:20 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,35 +1,41 @@
 import { db } from "./firebase.js";
 
 import {
-    doc,
-    updateDoc
+doc,
+updateDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const saveBtn = document.getElementById("save-btn");
 
 saveBtn.addEventListener("click", async () => {
 
-    try {
+try {
 
-        await updateDoc(
-            doc(db, "tracker", "live"),
-            {
-                status: document.getElementById("status").value,
-                message: document.getElementById("message").value,
-                battery: Number(document.getElementById("battery").value),
-                distance: document.getElementById("distance").value,
-                "last seen": document.getElementById("lastseen").value
-            }
-        );
+    await updateDoc(
+        doc(db, "tracker", "live"),
+        {
+            status: document.getElementById("status").value,
 
-        alert("Tracker Updated Successfully");
+            message: document.getElementById("message").value,
 
-    } catch (error) {
+            battery: Number(
+                document.getElementById("battery").value
+            ),
 
-        console.error(error);
+            distance: document.getElementById("distance").value,
 
-        alert("Update Failed");
+            lastSeen: document.getElementById("lastseen").value
+        }
+    );
 
-    }
+    alert("Tracker Updated Successfully");
+
+} catch (error) {
+
+    console.error(error);
+
+    alert("Update Failed");
+
+}
 
 });
